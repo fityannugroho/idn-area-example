954a965519b83522df3ffb88d503439cfaa519bf
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-03-16 15:57:12 +0700

    Update data-source.js
    - Add status constanta
    - Add getRegenciesByProvince()

--- src/scripts/data-source.js
+++ src/scripts/data-source.js
@@ -1,6 +1,12 @@
 export default class DataSource {
   static BASE_URL = 'https://idn-area.herokuapp.com';
 
+  static OK_STATUS = 200;
+
+  static BAD_REQUEST_STATUS = 400;
+
+  static NOT_FOUND_STATUS = 404;
+
   /**
    * Make a sort query.
    * @param {string} sortBy Sort the province by its `code` or `name`.
@@ -17,5 +23,14 @@ export default class DataSource {
       return Promise.reject(new Error('Failed to get provinces'));
     }
     return res.json();
+  }
+
+  static async getRegenciesByProvince(provinceCode) {
+    const res = await fetch(`${this.BASE_URL}/provinces/${provinceCode}/regencies`);
+    const resJson = await res.json();
+    if (!res.ok) {
+      return Promise.reject(new Error(resJson.message.join(', ')));
+    }
+    return resJson;
   }
 }
