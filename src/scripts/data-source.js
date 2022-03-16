6bd2f6ccb78ee21579aa977ae24a9bad9d7ede5a
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-03-16 17:00:13 +0700

    Add getDistrictsByRegency & docs improvement

--- src/scripts/data-source.js
+++ src/scripts/data-source.js
@@ -9,7 +9,7 @@ export default class DataSource {
 
   /**
    * Make a sort query.
-   * @param {string} sortBy Sort the province by its `code` or `name`.
+   * @param {string} sortBy Sort  by its `code` or `name`.
    * @param {string} sortOrder `asc` or `desc`.
    * @returns The sort query.
    */
@@ -17,6 +17,12 @@ export default class DataSource {
     return `?sortBy=${sortBy}&sortOrder=${sortOrder}`;
   }
 
+  /**
+   * Get all provinces
+   * @param {string} sortBy Sort the province by its `code` or `name`. The default is `code`.
+   * @param {string} sortOrder `asc` or `desc`.
+   * @returns The array of province.
+   */
   static async getProvinces(sortBy = 'code', sortOrder = 'asc') {
     const res = await fetch(`${this.BASE_URL}/provinces${DataSource.makeSortQuery(sortBy, sortOrder)}`);
     if (!res.ok) {
@@ -25,8 +31,31 @@ export default class DataSource {
     return res.json();
   }
 
-  static async getRegenciesByProvince(provinceCode) {
-    const res = await fetch(`${this.BASE_URL}/provinces/${provinceCode}/regencies`);
+  /**
+   * Get all regencies in a province.
+   * @param {string} provinceCode The province code.
+   * @param {string} sortBy Sort the regencies by its `code` or `name`. The default is `code`.
+   * @param {string} sortOrder `asc` or `desc`.
+   * @returns The array of regency.
+   */
+  static async getRegenciesByProvince(provinceCode, sortBy = 'code', sortOrder = 'asc') {
+    const res = await fetch(`${this.BASE_URL}/provinces/${provinceCode}/regencies${DataSource.makeSortQuery(sortBy, sortOrder)}`);
+    const resJson = await res.json();
+    if (!res.ok) {
+      return Promise.reject(new Error(resJson.message.join(', ')));
+    }
+    return resJson;
+  }
+
+  /**
+   * Get all districts in a regency.
+   * @param {string} regencyCode The regency code.
+   * @param {string} sortBy Sort the districts by its `code` or `name`. The default is `code`.
+   * @param {string} sortOrder `asc` or `desc`. The default is `asc`.
+   * @returns The array of district.
+   */
+  static async getDistrictsByRegency(regencyCode, sortBy = 'code', sortOrder = 'asc') {
+    const res = await fetch(`${this.BASE_URL}/regencies/${regencyCode}/districts${DataSource.makeSortQuery(sortBy, sortOrder)}`);
     const resJson = await res.json();
     if (!res.ok) {
       return Promise.reject(new Error(resJson.message.join(', ')));
