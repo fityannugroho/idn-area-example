a2978cd1226af4b38a2f85a38e6314b2f820c696
Fityan <fityannugroho@gmail.com>
2023-05-15 14:36:26 +0700

    fix: update title & nav links

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -8,20 +8,20 @@ class NavBar extends HTMLElement {
   render() {
     $(this).html(`
       <div class="container-fluid px-4">
-        <a class="navbar-brand" href="#">idn-area Example</a>
+        <a class="navbar-brand" href="#">idn-area</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <i class="fa-solid fa-bars fa-xl"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" href="https://idn-area.herokuapp.com"  target="_blank" rel="noopener noreferrer">API</a>
+              <a class="nav-link" href="https://idn-area.up.railway.app"  target="_blank" rel="noopener noreferrer">API</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="https://github.com/fityannugroho/idn-area#readme"  target="_blank" rel="noopener noreferrer">Docs</a>
+              <a class="nav-link" href="https://github.com/fityannugroho/idn-area"  target="_blank" rel="noopener noreferrer">Docs</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="https://fityannugroho.github.io"  target="_blank" rel="noopener noreferrer">About Me</a>
+              <a class="nav-link" href="https://fityannugroho.vercel.app"  target="_blank" rel="noopener noreferrer">About Me</a>
             </li>
           </ul>
         </div>
